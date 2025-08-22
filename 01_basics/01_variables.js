0571d20eaf1923af5ff78a76bafffffb79c06e6e
MuhammadSaim-razzaq <saimrazzaq1103@gmail.com>
2025-08-22 18:07:18 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = "24k-0704"
+let accountEmail = "saimrazzaq@gmail.com"
+var accountPassword = "123"
+accountCity = "Karachi"
+
+//accountId = 2
+console.log(accountId);
+
+accountEmail = "123@gmail.com"
+accountPassword= "234"
+accountCity = "Isb"
+let accountstate
+
+console.table([accountEmail,accountPassword,accountCity,accountstate]);
+
+/*prefer not to use var because of block scope and functional scope issue */
+ 
